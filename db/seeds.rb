a4ceee3e547c00cee7a2f49a1a017de1e2426365
Jack Aubone <jfaubone@gmail.com>
2022-08-31 11:49:45 -0500

    Created products controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+product1 = Product.create(name: "Giant Rasta Banana", price: 100, image_url:"https://img.5milesapp.com/image/upload/f_auto,t_i800/v1481413844/wvcqca8bhrs3x443pyya.jpg", description: "A giant rasta banana, the perfect best friend.")
+
+#product1.save
+p product1
