3d58832ba3adeedbe373da48b9dc97911285c392
Jack Aubone <jfaubone@gmail.com>
2022-09-05 13:36:40 -0500

    Using new migrations to change and add to or delete from tables

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+
+  def is_discounted?
+    if price <= 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+      return price.to_f * 0.09
+  end
+
+  def total
+    return tax + price
+  end
 end
